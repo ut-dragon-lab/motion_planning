e22fadede4c3f5dd4ccd72f3411139a893600357
Takuzumi240 <nishio@jsk.imi.i.u-tokyo.ac.jp>
2019-11-27 20:43:19 +0900

    fix FlightNav.h location from aerial_robot_base to aerial_robot_msgs

--- aerial_manipulation/aerial_transportation/include/aerial_transportation/grasp_control/base.h
+++ aerial_manipulation/aerial_transportation/include/aerial_transportation/grasp_control/base.h
@@ -5,7 +5,7 @@
 /* ros */
 #include <ros/ros.h>
 
-#include <aerial_robot_base/FlightNav.h>
+#include <aerial_robot_msgs/FlightNav.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Point.h>
