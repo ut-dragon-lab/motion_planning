73d23acdf7c46f5b93f8a76f3af22e6a4b8f1783
moju zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2020-05-06 22:00:44 +0900

    Refactor the jacobian for rotor overlapping

--- aerial_motion/differential_kinematics/src/constraint/se3/overlap.cpp
+++ aerial_motion/differential_kinematics/src/constraint/se3/overlap.cpp
@@ -48,7 +48,7 @@ namespace differential_kinematics
     class Overlap :public Base
     {
     public:
-      Overlap(): result_overlap_min_(1e6)
+      Overlap(): overlap_min_(1e6)
       {}
       ~Overlap(){}
 
@@ -59,132 +59,47 @@ namespace differential_kinematics
       {
         Base::initialize(nh, nhp, planner, constraint_name, orientation, full_body);
 
-        nc_ = 1; // temporary, using one dimension
+        nc_ = 1; // using one dimension
 
-        nhp_.param ("overlap_change_vel_thre", overlap_change_vel_thre_, 0.05);
-        if(verbose_) std::cout << "overlap_change_vel_thre: " << std::setprecision(3) << overlap_change_vel_thre_ << std::endl;
-        nhp_.param ("overlap_constraint_range", overlap_constraint_range_, 0.05); // [m]
-        if(verbose_) std::cout << "overlap_constraint_range: " << std::setprecision(3) << overlap_constraint_range_ << std::endl;
-        nhp_.param ("overlap_forbidden_range", overlap_forbidden_range_, 0.01); // [m]
-        if(verbose_) std::cout << "overlap_forbidden_range: " << std::setprecision(3) << overlap_forbidden_range_ << std::endl;
-
+        getParam<double>("overlap_change_vel_thre", overlap_change_vel_thre_, 0.05);
+        getParam<double>("overlap_constraint_range", overlap_constraint_range_, 0.05); // [m]
+        getParam<double>("overlap_forbidden_range", overlap_forbidden_range_, 0.01); // [m]
       }
 
       bool getConstraint(Eigen::MatrixXd& A, Eigen::VectorXd& lb, Eigen::VectorXd& ub, bool debug = false)
       {
-        /* TODO: not general */
-        auto dragon_model_ptr = boost::dynamic_pointer_cast<DragonRobotModel>(planner_->getRobotModelPtr());
+        auto robot_model = boost::dynamic_pointer_cast<DragonRobotModel>(planner_->getRobotModelPtr());
+        const auto joint_positions = planner_->getTargetJointVector<KDL::JntArray>();
 
-        auto robotModelUpdate = [this, &dragon_model_ptr](KDL::Rotation root_att, KDL::JntArray joint_vector)
-          {
-            dragon_model_ptr->setCogDesireOrientation(root_att);
-            dragon_model_ptr->updateRobotModel(joint_vector);
-            return dragon_model_ptr->getEdfsOriginFromCog<Eigen::Vector3d>();
-          };
+        double rotor_diameter = 2 * robot_model->getEdfRadius();
+        double min_dist = robot_model->getClosestRotorDist() - rotor_diameter;
+        int rotor_i = robot_model->getClosestRotorIndices().at(0);
+        int rotor_j = robot_model->getClosestRotorIndices().at(1);
+        const std::vector<Eigen::Vector3d> rotor_pos = robot_model->getEdfsOriginFromCog<Eigen::Vector3d>();
+        const auto& rotor_names = robot_model->getEdfNames();
+        double tan_max_tilt = tan(robot_model->getEdfMaxTilt());
 
-        auto distCalc = [this, &dragon_model_ptr](std::vector<Eigen::Vector3d> edfs_origin_from_cog,
-                           int link_i, int link_j)
-          {
-            Eigen::Vector3d diff3d = edfs_origin_from_cog[link_i] - edfs_origin_from_cog[link_j]; //dual
-            double projected_dist2d = sqrt(diff3d(0) * diff3d(0) + diff3d(1) * diff3d(1));
-            /* Note: approximated, the true one should be (edf_radius_ / cos(tilt) + diff(2) * tan(tilt) */
-            double dist2d_thre = 2 * dragon_model_ptr->getEdfRadius()  + fabs(diff3d(2)) * tan(dragon_model_ptr->getEdfMaxTilt());
 
-            return projected_dist2d - dist2d_thre;
-          };
-
-        A = Eigen::MatrixXd::Zero(nc_, planner_->getRobotModelPtr()->getLinkJointIndices().size() + 6);
+        Eigen::MatrixXd jacobian = robot_model->getRotorOverlapJacobian();
+        A = jacobian;
         lb = Eigen::VectorXd::Constant(nc_, -overlap_change_vel_thre_);
         ub = Eigen::VectorXd::Constant(nc_, 1e6);
 
-        auto curr_root_att = planner_->getTargetRootPose<KDL::Frame>().M;
-        auto curr_joint_vector =  planner_->getTargetJointVector<KDL::JntArray>();
-        auto edfs_origin_from_cog = dragon_model_ptr->getEdfsOriginFromCog<Eigen::Vector3d>();
+        lb(0) = damplingBound(min_dist, -overlap_change_vel_thre_, overlap_constraint_range_, overlap_forbidden_range_);
 
-        /* Note: we only choose the minimum diff, because it is not feasible for N-multilink */
-        double min_dist = 1e6;
-        int overlap_link1, overlap_link2;
-        for(int i = 0; i < rotor_num_ * 2; i++)
-          {
-            for(int j =  i + 1; j < rotor_num_ * 2; j++)
-              {
-                /* special for dual rotor */
-                if(i / 2 == j / 2) continue;
-
-                auto dist = distCalc(edfs_origin_from_cog, i, j);
-
-                if(dist < 0)
-                  {
-                    if(verbose_)ROS_ERROR("constraint: %s, overlap!: %d and %d, dist: %f", constraint_name_.c_str(), i + 1, j + 1, dist);
-
-                    /* revert the robot model with current state */
-                    robotModelUpdate(curr_root_att, curr_joint_vector);
-
-                    return false;
-                  }
-
-                if(dist < min_dist)
-                  {
-                    min_dist = dist;
-                    overlap_link1 = i;
-                    overlap_link2 = j;
-                  }
-              }
-          }
-
-        if(debug)ROS_INFO("constraint: %s, overlap min dist %f between %d and %d", constraint_name_.c_str(), min_dist, overlap_link1 + 1, overlap_link2 + 1);
-
-        /* update matrix A */
-        double delta_angle = 0.001; // [rad]
-        /* joint */
-        for(int index = 0; index < planner_->getRobotModelPtr()->getLinkJointIndices().size(); index++)
-          {
-            KDL::JntArray perturbation_joint_vector = curr_joint_vector;
-            perturbation_joint_vector(planner_->getRobotModelPtr()->getLinkJointIndices().at(index)) += delta_angle;
-
-            edfs_origin_from_cog = robotModelUpdate(curr_root_att,
-                                                    perturbation_joint_vector);
-            auto dist = distCalc(edfs_origin_from_cog, overlap_link1, overlap_link2);
-            A(0, 6 + index) = (dist - min_dist) / delta_angle;
-          }
-
-        if(full_body_)
-          {
-            /* roll */
-            edfs_origin_from_cog = robotModelUpdate(curr_root_att * KDL::Rotation::RPY(delta_angle, 0, 0), curr_joint_vector);
-            auto dist = distCalc(edfs_origin_from_cog, overlap_link1, overlap_link2);
-            A(0, 3) = (dist - min_dist) / delta_angle;
-
-            /* pitch */
-            edfs_origin_from_cog = robotModelUpdate(curr_root_att * KDL::Rotation::RPY(0, delta_angle, 0), curr_joint_vector);
-            dist = distCalc(edfs_origin_from_cog, overlap_link1, overlap_link2);
-            A(0, 4) = (dist - min_dist) / delta_angle;
-
-            /* yaw */
-            edfs_origin_from_cog = robotModelUpdate(curr_root_att * KDL::Rotation::RPY(0, 0, delta_angle), curr_joint_vector);
-            dist = distCalc(edfs_origin_from_cog, overlap_link1, overlap_link2);
-            A(0, 5) = (dist - min_dist) / delta_angle;
-          }
-        /* set lb */
-        if(min_dist < overlap_constraint_range_)
-            lb(0) *= ((min_dist - overlap_forbidden_range_) / (overlap_constraint_range_ - overlap_forbidden_range_));
-
-        //lb(0) = -1e6;
         if(debug)
           {
+            ROS_INFO("constraint: %s, overlap min dist %f between %d and %d", constraint_name_.c_str(), min_dist, rotor_i, rotor_j);
             std::cout << "constraint (" << constraint_name_ << "): matrix A: \n" << A <<  "\n lb: \n" << lb.transpose() << "\n, ub: \n" << ub.transpose() << std::endl;
           }
 
         /* update reuslt */
-        if(result_overlap_min_ > min_dist)
+        if(overlap_min_ > min_dist)
           {
-            result_overlap_min_ = min_dist;
-            result_overlap_link1_ = overlap_link1;
-            result_overlap_link2_ = overlap_link2;
+            overlap_min_ = min_dist;
+            rotor_i_ = rotor_i;
+            rotor_j_ = rotor_j;
           }
-
-        /* revert the robot model with current state */
-        robotModelUpdate(curr_root_att, curr_joint_vector);
 
         return true;
       }
@@ -192,9 +107,9 @@ namespace differential_kinematics
       void result()
       {
         std::cout << constraint_name_ << "\n"
-                  << "result overlap min: " << result_overlap_min_
-                  << " at rotor" << result_overlap_link1_
-                  << " at rotor" << result_overlap_link2_
+                  << "result overlap min: " << overlap_min_
+                  << " at rotor" << rotor_i_
+                  << " at rotor" << rotor_j_
                   << std::endl;
       }
 
@@ -205,9 +120,9 @@ namespace differential_kinematics
       double overlap_constraint_range_;
       double overlap_forbidden_range_;
 
-      double result_overlap_min_;
-      int result_overlap_link1_;
-      int result_overlap_link2_;
+      double overlap_min_;
+      int rotor_i_;
+      int rotor_j_;
     };
   };
 };
