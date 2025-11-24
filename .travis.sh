b0aaef4fdadca08e939b16ba43595db6c68b3346
Moju Zhao <tongtybj@gmail.com>
2025-11-24 22:26:57 +0900

    [GA] support to test ros-one in 22.04 and ros-noetic in ARM

--- .travis.sh
+++ .travis.sh
@@ -5,24 +5,29 @@ set -ex
 apt-get update -qq && apt-get install -y -q curl wget sudo lsb-release gnupg git sed build-essential # for docker
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
-echo "Testing branch $TRAVIS_BRANCH of $REPOSITORY_NAME"
-sudo sh -c "echo \"deb ${REPOSITORY} `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt-get update -qq
-
 # Install ROS
-if [[ "$ROS_DISTRO" ==  "noetic" ]]; then
-    sudo apt-get install -y -q python3-catkin-pkg python3-catkin-tools python3-rosdep python3-wstool python3-rosinstall-generator python3-osrf-pycommon python-is-python3
+if [[ "$ROS_DISTRO" ==  "one" ]]; then
+    git clone https://github.com/jsk-ros-pkg/jsk_aerial_robot.git
+    cd jsk_aerial_robot && ./configure.sh
 else
-    sudo apt-get install -y -q python-catkin-pkg python-catkin-tools python-rosdep python-wstool python-rosinstall-generator
+    sudo sh -c "echo \"deb ${REPOSITORY} `lsb_release -cs` main\" > /etc/apt/sources.list.d/ros-latest.list"
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt-get update -qq
+
+    if [[ "$ROS_DISTRO" ==  "noetic" ]]; then
+        sudo apt-get install -y -q python3-catkin-pkg python3-catkin-tools python3-rosdep python3-wstool python3-rosinstall-generator python3-osrf-pycommon python-is-python3
+    else
+        sudo apt-get install -y -q python-catkin-pkg python-catkin-tools python-rosdep python-wstool python-rosinstall-generator
+    fi
+    sudo apt-get install -y -q ros-$ROS_DISTRO-catkin
+
+    # Setup for rosdep
+    sudo rosdep init
+    rosdep update --include-eol-distros
 fi
-sudo apt-get install -y -q ros-$ROS_DISTRO-catkin
+
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
-
-# Setup for rosdep
-sudo rosdep init
-rosdep update --include-eol-distros
 
 # Install source code
 mkdir -p ~/catkin_ws/src
